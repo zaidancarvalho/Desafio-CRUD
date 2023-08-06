27eb0eaa7a06258dd889b25aaf9e2025b213d579
Felipe Carvalho <felipecfernandez@hotmail.com>
2023-08-06 18:04:37 -0300

    Client entity

---
+++ src/main/java/com/devsuperior/desafio/entity/Client.java
@@ -0,0 +1,74 @@
+package com.devsuperior.desafio.entity;
+
+import java.time.LocalDate;
+
+public class Client {
+
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private LocalDate birthDate;
+	private Integer children;
+	
+	public Client() {
+	}
+
+	public Client(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+}
