89af0c12c4f8ea27dae86b7cf17e53fd433d6fec
Felipe Carvalho <felipecfernandez@hotmail.com>
2023-08-06 19:06:18 -0300

    GET clients paged

--- src/main/java/com/devsuperior/desafio/controller/ClientController.java
+++ src/main/java/com/devsuperior/desafio/controller/ClientController.java
@@ -1,9 +1,14 @@
 package com.devsuperior.desafio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.desafio.entity.Client;
 import com.devsuperior.desafio.service.ClientService;
 
 @RestController
@@ -13,5 +18,10 @@ public class ClientController {
 	@Autowired
 	private ClientService service;
 	
+	@GetMapping
+	public ResponseEntity <Page<Client>> findAll(Pageable pageable) {
+		Page<Client> client = service.findAll(pageable);
+		return ResponseEntity.ok().body(client);
+	}
 	
 }
--- src/main/java/com/devsuperior/desafio/service/ClientService.java
+++ src/main/java/com/devsuperior/desafio/service/ClientService.java
@@ -1,8 +1,11 @@
 package com.devsuperior.desafio.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.desafio.entity.Client;
 import com.devsuperior.desafio.repository.ClientRepository;
 
 @Service
@@ -10,4 +13,11 @@ public class ClientService {
 
 	@Autowired
 	private ClientRepository repository;
+	
+	
+	public Page<Client> findAll(Pageable pageable) {
+		Page<Client> result = repository.findAll(pageable);
+		return result.map(x -> new Client(x.getId(), x.getName(), x.getCpf(), x.getIncome(), x.getBirthDate(), x.getChildren()));
+	}
+
 }
