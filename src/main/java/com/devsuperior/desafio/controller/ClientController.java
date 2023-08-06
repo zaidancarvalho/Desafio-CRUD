a82852470a98810315bc6dddf39d3c5a2f8d2c7e
Felipe Carvalho <felipecfernandez@hotmail.com>
2023-08-06 19:19:51 -0300

    GET client by id

--- src/main/java/com/devsuperior/desafio/controller/ClientController.java
+++ src/main/java/com/devsuperior/desafio/controller/ClientController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,9 @@ public class ClientController {
 		return ResponseEntity.ok().body(client);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity <Client> findById(@PathVariable Long id) {
+		Client client =  service.findById(id);
+		return ResponseEntity.ok().body(client);
+	}
 }
--- src/main/java/com/devsuperior/desafio/service/ClientService.java
+++ src/main/java/com/devsuperior/desafio/service/ClientService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.desafio.entity.Client;
 import com.devsuperior.desafio.repository.ClientRepository;
 
+import org.springframework.transaction.annotation.Transactional;
+
 @Service
 public class ClientService {
 
@@ -15,9 +17,16 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	
+	@Transactional(readOnly = true)
 	public Page<Client> findAll(Pageable pageable) {
 		Page<Client> result = repository.findAll(pageable);
 		return result.map(x -> new Client(x.getId(), x.getName(), x.getCpf(), x.getIncome(), x.getBirthDate(), x.getChildren()));
+	}
+	
+	@Transactional(readOnly = true)
+	public Client findById(Long id) {
+		Client client =  repository.findById(id).get();
+		return client;
 	}
 
 }
